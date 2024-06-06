10d324ab6924148685a5b59c0dc7ebdd3a3d7d98
Sias27 <sias27@qq.com>
2024-06-06 18:56:49 +0800

    Removed temporary files

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/sias/Desktop/PL0/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/sias/Desktop/PL0_Complier/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
